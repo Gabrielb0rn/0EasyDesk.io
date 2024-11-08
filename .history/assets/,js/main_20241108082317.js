86218a50f85b39dd52d850329435648ff21a95a8
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-11-08 08:24:34 -0300

    .

--- .history/assets/,js/main_20241108082317.js
+++ .history/assets/,js/main_20241108082317.js
@@ -3,6 +3,7 @@ function toggleTheme() {
     document.body.classList.toggle('dark-theme');
 }
 
+// Adicione aqui qualquer inicialização global, se necessário
 document.addEventListener('DOMContentLoaded', () => {
     // Código de inicialização global
 });
