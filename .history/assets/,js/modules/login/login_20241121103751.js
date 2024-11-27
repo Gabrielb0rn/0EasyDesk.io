cde749cb74217f501925bea99401410bc485061b
Gabrielb0rn <161833929+Gabrielb0rn@users.noreply.github.com>
2024-11-27 08:34:03 -0300

    .

--- .history/assets/,js/modules/login/login_20241121103751.js
+++ .history/assets/,js/modules/login/login_20241121103751.js
@@ -28,7 +28,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
         sessionStorage.setItem('userRole', storedPredefinedUser.email === email ? 'admin' : 'user');
 
         alert('Login bem-sucedido!');
-        window.location.href = 'index.html';
+        window.location.href = 'admin.html';
     } else {
         alert('Email ou senha incorretos.');
     }
